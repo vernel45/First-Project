bb6e606fc1df15cc7932a3919dc9ed74921d66ad
Sam Verneuille <samvernel268@gmail.com>
2026-06-29 14:03:59 -0400

    fix mobile menu

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('wekjfbjwebf')
+
+
+function openMenu() {
+	document.body.classList += "menu--open"
+}
+
+function closeMenu() {
+	body.classList.remove('menu--open');
+}
+
+if (menuButton && closeButton) {
+	menuButton.addEventListener('click', openMenu);
+	closeButton.addEventListener('click', closeMenu);
+}
+
+menuLinks.forEach((link) => {
+	link.addEventListener('click', closeMenu);
+});
